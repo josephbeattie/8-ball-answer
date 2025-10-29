292c6c639db72d8cab7e8af120b70a04251bd4ac
Joseph Beattie <74561130+josephbeattie@users.noreply.github.com>
2025-10-29 15:57:48 +0000

    Added background colour chooser
    User can now switch from dark to light bg

--- 8 Ball/ContentView.swift
+++ 8 Ball/ContentView.swift
@@ -11,27 +11,42 @@ struct ContentView: View {
     @State private var fortune = "Tap for answer"
     @State private var scale: CGFloat = 1.0
     @State private var answersModel: AnswersModel = .init()
+    
+    @AppStorage("darkBG") var isDark: Bool = false
 
     var body: some View {
-        Text(fortune)
-            .font(.system(size: 500))  // Start with a very large size
-            .fontWeight(.bold)
-            .foregroundColor(.white)
-            .multilineTextAlignment(.center)
-            .minimumScaleFactor(0.01)  // Allow scaling down to 1% if needed
-            .lineLimit(nil)
-            .scaleEffect(scale, anchor: UnitPoint.center)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .padding()  // Add padding to prevent edge touching
-            .background(Color.black)
-            .edgesIgnoringSafeArea(.all)
-            .onTapGesture {
-                fortune = answersModel.getAnswer()
-                self.scale = 1.8
-                withAnimation(.easeIn(duration: 0.2)) {
-                    self.scale = 1.0
+        TabView{
+            Tab("Home", systemImage: "house"){
+                Text(fortune)
+                    .font(.system(size: 500))  // Start with a very large size
+                    .fontWeight(.bold)
+                    .foregroundColor(isDark ? .white :.black)
+                    .multilineTextAlignment(.center)
+                    .minimumScaleFactor(0.01)  // Allow scaling down to 1% if needed
+                    .lineLimit(nil)
+                    .scaleEffect(scale, anchor: UnitPoint.center)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding()  // Add padding to prevent edge touching
+                    .background(isDark ? .black : .white)
+                    .edgesIgnoringSafeArea(.all)
+                    .onTapGesture {
+                        fortune = answersModel.getAnswer()
+                        self.scale = 1.8
+                        withAnimation(.easeIn(duration: 0.2)) {
+                            self.scale = 1.0
+                        }
+                    }
+            }
+            Tab("Settings", systemImage: "gear"){
+                VStack{
+                    Form{
+                        Text("Little Settings Test")
+                        Toggle("Show dark background", isOn: $isDark)
+                    }
+                    
                 }
             }
+        }
     }
 }
 
